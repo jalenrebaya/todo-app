88206fcb5658cd529aa76473b49dff8263ebf0f2
Jallyn Mapangit Ribaya <jallynribaya@gmail.com>
2026-08-27 10:02:32 +0800

    Add Express backend

--- server/server.js
+++ server/server.js
@@ -1,34 +1,42 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
-var cors = require('cors')
-const port = 3000;
 
-
-// Adds headers: Access-Control-Allow-Origin
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-
 let todos = [
- { id: 1, text: "Learn Express", completed: false },
- { id: 2, text: "Build a REST API", completed: false }
+    {
+        id: 1,
+        text: "Learn Express",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Build a REST API",
+        completed: false
+    }
 ];
 
-app.get('/api/todos', (req, res) => {
-  res.json(todos);
+app.get("/api/todos", (req, res) => {
+    res.json(todos);
 });
 
-app.post('/api/todos', (req, res) => { 
-   const newTodo = {
-    id: Date.now(),
-    text: req.body.text,
-    completed: false
-  };
+app.post("/api/todos", (req, res) => {
+    const newTodo = {
+        id: Date.now(),
+        text: req.body.text,
+        completed: false
+    };
 
-  todos.push(newTodo);
-  res.send(todos)
-}); 
+    todos.push(newTodo);
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+    res.json(newTodo);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
